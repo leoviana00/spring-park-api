bda923ecb8d616e39770929d16d4820dd72bbed4
Leonardo Viana <leonardo.viana@grupomateus.com>
2024-08-29 10:03:22 -0300

    feat(base-project): add base project springboot

---
+++ src/main/java/com/viana/sprint_park_api/SprintParkApiApplication.java
@@ -0,0 +1,13 @@
+package com.viana.sprint_park_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintParkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintParkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viana/sprint_park_api/SprintParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viana.sprint_park_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
