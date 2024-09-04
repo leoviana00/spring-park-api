cc91e74c05fde18949f037c606c2cc0387b3cd72
Leonardo Viana <leonardo.viana@grupomateus.com>
2024-09-04 06:41:42 -0300

    feat(modules): Usuario Service - Criando metodos salvar, buscar por id, editar password e buscar todos

---
+++ src/main/java/com/viana/sprint_park_api/service/UsuarioService.java
@@ -0,0 +1,43 @@
+package com.viana.sprint_park_api.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.viana.sprint_park_api.entity.Usuario;
+import com.viana.sprint_park_api.repository.UsuarioRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+            () -> new RuntimeException("Usuário não encontrado. ")
+        );
+    }
+
+    @Transactional
+    public Usuario editarPassword(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
+}
