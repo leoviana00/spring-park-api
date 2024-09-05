58d0746ab02a4d38a3da6b4372d4693a45f0fe6e
Leonardo Viana <leonardo.viana@grupomateus.com>
2024-09-05 08:25:42 -0300

    feat(dto): usuario mapper

---
+++ src/main/java/com/viana/sprint_park_api/web/dto/mapper/UsuarioMapper.java
@@ -0,0 +1,32 @@
+package com.viana.sprint_park_api.web.dto.mapper;
+
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+
+
+
+import com.viana.sprint_park_api.entity.Usuario;
+import com.viana.sprint_park_api.web.dto.UsuarioCreateDto;
+import com.viana.sprint_park_api.web.dto.UsuarioResponseDto;
+
+
+public class UsuarioMapper {
+
+    public static Usuario toUsuario(UsuarioCreateDto createDto) {
+        return new ModelMapper().map(createDto, Usuario.class);
+    }
+
+    public static UsuarioResponseDto toDto(Usuario usuario) {
+        String role = usuario.getRole().name().substring("ROLE_".length());
+        PropertyMap<Usuario, UsuarioResponseDto> props = new PropertyMap<Usuario, UsuarioResponseDto>() {
+            @Override
+            protected void configure() {
+                map().setRole(role);
+            }
+        };
+        ModelMapper mapper = new ModelMapper();
+        mapper.addMappings(props);
+        return mapper.map(usuario, UsuarioResponseDto.class);
+    }
+}
