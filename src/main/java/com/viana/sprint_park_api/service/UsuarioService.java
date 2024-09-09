dead2f21a670a86b232c0df5379315c9fd407402
Leonardo Viana <leonardo.viana@grupomateus.com>
2024-09-09 09:11:33 -0300

    feat(usuarioSenhaDto): mapeamento de objeto senha usuario

--- src/main/java/com/viana/sprint_park_api/service/UsuarioService.java
+++ src/main/java/com/viana/sprint_park_api/service/UsuarioService.java
@@ -28,9 +28,15 @@ public class UsuarioService {
     }
 
     @Transactional
-    public Usuario editarPassword(Long id, String password) {
+    public Usuario editarPassword(Long id, String senhaAtual, String novaSenha, String confirmaSenha) {
+        if(!novaSenha.equals(confirmaSenha)){
+            throw new RuntimeException("Nova senha não confere com a confirmação de senha!");
+        }
         Usuario user = buscarPorId(id);
-        user.setPassword(password);
+        if(!user.getPassword().equals(senhaAtual)){
+            throw new RuntimeException("Sua senha não confere!");        }
+
+        user.setPassword(novaSenha);
         return user;
 
     }
--- src/main/java/com/viana/sprint_park_api/web/UsuarioController.java
+++ src/main/java/com/viana/sprint_park_api/web/UsuarioController.java
@@ -15,6 +15,7 @@ import com.viana.sprint_park_api.entity.Usuario;
 import com.viana.sprint_park_api.service.UsuarioService;
 import com.viana.sprint_park_api.web.dto.UsuarioCreateDto;
 import com.viana.sprint_park_api.web.dto.UsuarioResponseDto;
+import com.viana.sprint_park_api.web.dto.UsuarioSenhaDto;
 import com.viana.sprint_park_api.web.dto.mapper.UsuarioMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -33,20 +34,20 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+    public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
-        Usuario user = usuarioService.editarPassword(id, usuario.getPassword());
-        return ResponseEntity.ok(user);
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UsuarioSenhaDto dto) {
+        Usuario user = usuarioService.editarPassword(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
-    public ResponseEntity<List<Usuario>> getAll() {
+    public ResponseEntity<List<UsuarioResponseDto>> getAll() {
         List<Usuario> users = usuarioService.buscarTodos();
-        return ResponseEntity.ok(users);
+        return ResponseEntity.ok(UsuarioMapper.toListDto(users));
     }
 }
