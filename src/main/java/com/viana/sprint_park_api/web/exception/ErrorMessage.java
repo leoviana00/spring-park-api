85ee5956cf2520460a88ef8d40a6239d1dc32226
Leonardo Viana <leonardo.viana@grupomateus.com>
2024-09-15 10:45:09 -0300

    feat(error): tratamento de excecoes - error

---
+++ src/main/java/com/viana/sprint_park_api/web/exception/ErrorMessage.java
@@ -0,0 +1,56 @@
+package com.viana.sprint_park_api.web.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ErrorMessage {
+
+    private String path;
+    private String method;
+    private int status;
+    private String statusText;
+    private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, String> erros;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErros(result);
+    }
+
+    private void addErros(BindingResult result){
+        this.erros = new HashMap<>();
+        for(FieldError fieldError : result.getFieldErrors()) {
+            this.erros.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+
+    
+}
