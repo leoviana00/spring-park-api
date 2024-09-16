9c2de9d5b0aa5f68418eef1df7c6edb50b9811ae
Leonardo Viana <leonardo.viana@grupomateus.com>
2024-09-16 09:39:23 -0300

    feat(testes): add inscripts para inicializar dados no banco

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS (id, username, password, role) values (100, 'teste1@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'teste2@email.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (102, 'teste3@email.com', '123456', 'ROLE_CLIENTE');
