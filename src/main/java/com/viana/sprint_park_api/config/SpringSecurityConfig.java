7f2d8d8a908acbcffd8bdf495adb74c6da481a64
Leonardo Viana <leonardo.viana@grupomateus.com>
2024-09-17 11:14:54 -0300

    feat(openapi-swagger): liberacao de acesso aos recursos da documentação [BUG] - Rota do swagger não está abrindo #28

--- src/main/java/com/viana/sprint_park_api/config/SpringSecurityConfig.java
+++ src/main/java/com/viana/sprint_park_api/config/SpringSecurityConfig.java
@@ -14,12 +14,21 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.viana.sprint_park_api.jwt.JwtAuthenticationEntryPoint;
 import com.viana.sprint_park_api.jwt.JwtAuthorizationFilter;
 
 @EnableMethodSecurity
 @EnableWebMvc
 @Configuration
 public class SpringSecurityConfig {
+
+    private static final String[] DOCUMENTATION_OPENAPI = {
+        "/docs/index.html",
+        "/docs-park.html", "/docs-park/**",
+        "/v3/api-docs/**",
+        "/swagger-ui-custom.html", "/swagger-ui.html", "/swagger-ui/**",
+        "/**.html", "/webjars/**", "/configuration/**", "/swagger-resources/**"
+};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,11 +39,15 @@ public class SpringSecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "api/v1/usuarios").permitAll()
                         .requestMatchers(HttpMethod.POST, "api/v1/auth").permitAll()
+                        .requestMatchers(DOCUMENTATION_OPENAPI).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/actuator/*").permitAll()
                         .anyRequest().authenticated()
                 ).sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).addFilterBefore(
                         jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class
+                ).exceptionHandling(ex -> ex
+                    .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
                 ).build();
     }
 
